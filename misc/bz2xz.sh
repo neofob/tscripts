72aeb514e46a1b36c6e9f4f94c1346eaba13f227
Tuan T. Pham <tuan@vt.edu>
2016-03-27 10:56:32 -0400

    Correct RE in bz2xz
    misc/bz2xz.sh:
    	* Update RE

--- misc/bz2xz.sh
+++ misc/bz2xz.sh
@@ -7,7 +7,7 @@ CPUS=${CPUS:=4}
 RM=${RM:=0}
 
 for i in $@; do
-	OUT_FILE=`echo $i | sed 's/bz.\+$/xz/'`
+	OUT_FILE=`echo $i | sed 's/bz.\?$/xz/'`
 	echo "Converting $i to $OUT_FILE"
 	time bzcat $i | pv | pxz -T$CPUS -c9 - > $OUT_FILE
 	[ "$RM" -eq 1 ] && (echo "Removing $i"; rm $i)
