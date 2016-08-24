549f617f07ac16a893e82aadfd0b8ef8ca0ef5eb
Tuan T. Pham <tuan@vt.edu>
2016-08-23 21:45:22 -0400

    Update zerofree help message
    blkdev/zerofree_vdi.sh:
    	* Rewrite the way help message is printed

--- blkdev/zerofree_vdi.sh
+++ blkdev/zerofree_vdi.sh
@@ -2,16 +2,11 @@
 # Mount virtual disk image, vdi and what not, as an nbd
 # then zerofree it, assuming that the filesystem is ext[3-4]
 # Just run this script as root so it doesnt have to sudo
-
 # __author__: tuan t. pham <tuan at vt dot edu>
-
-# Requirement package(s) (debian/ubuntu):
-# apt-get install qemu-utils
-# modprobe nbd max_part=16
 
 NBD_DEV=${NBD_DEV:="/dev/nbd0"}
 VPART=${VPART:=1}
-ZEROFREE="zerofree"
+ZEROFREE=${ZEROFREE:=zerofree}
 NBD_CMD_C="qemu-nbd -c"
 NBD_CMD_D="qemu-nbd -d"
 
@@ -21,50 +16,56 @@ NBD_CMD_D="qemu-nbd -d"
 # file resides on a SSD or non-spinning disk
 # ZEROFREE_OPTS="-t 4"
 
-function help_msg
+help_msg="\e[1;31mUsage:\e[0m $0 <VirtualDiskImage>
+	zerofree a virtual disk using nbd, Network Block Device, loopback mounting
+	Run this script with sudo or as \e[1;31mroot\e[0m
+
+\e[1;31mEnvironment variables:\e[0m
+	NBD_DEV Network Block Device to be used
+		e.g. /dev/nbd0, /dev/nbd1
+		DEFAULT NBD_DEV=/dev/nbd0
+
+\e[1;31mExamples:\e[0m
+	0) Use the default /dev/nbd0 for virtual image disk
+	# $0 Jessie.vdi
+
+	1) Use nbd2 as a nbd loopback device and zerofree the 2nd partion
+	# NBD_DEV=/dev/nbd2 VPART=2 $0 Jessie.vdi
+
+\e[1;33mRequirement package(s) (debian/ubuntu):\e[0m
+	# apt-get install -yq qemu-utils zerofree
+	# modprobe nbd max_part=16
+
+__author__: tuan t. pham"
+
+function print_help()
 {
-	echo "Usage: $0 <VirtualDiskImage>"
-	echo -e "\tzerofree a virtual disk using nbd loopback mounting"
-	echo -e "\tRun this script with sudo or as root"
-	echo
-	echo "Environment variables:"
-	echo -e "\tNBD_DEV Network Block Device to be used"
-	echo -e "\t\te.g. /dev/nbd0, /dev/nbd1"
-	echo -e "\t\tDEFAULT NBD_DEV=/dev/nbd0"
-	echo
-	echo "Examples:"
-	echo -e "\t1) Use the default /dev/nbd0 for virtual image disk"
-	echo -e "\t# $0 Jessie.vdi"
-	echo
-	echo -e "\t2) Use nbd2 as a nbd loopback device and zerofree the 2nd partion"
-	echo -e "\t# NBD_DEV=/dev/nbd2 VPART=2 sudo $0 Jessie.vdi"
-	echo
-	echo "__author__: tuan t. pham"
+	exec echo -e "$help_msg"
 }
 
 # e.g.: mount_nbd /dev/nbd0 jessie.vdi
 # Now we have /dev/nbd0 block device as well ass /dev/nbd0p* partition(s)
-function mount_nbd
+function mount_nbd()
 {
 	echo "Entering mount_nbd $1 $2"
 	eval "$NBD_CMD_C $1 $2"
 }
 
 # e.g.: umount_nbd /dev/nbd0
-function umount_nbd
+function umount_nbd()
 {
 	echo "Entering umount_nbd $1"
 	eval "$NBD_CMD_D $1"
 }
 
 # e.g.: zerofree_func /dev/nbd0p1
-function zerofree_func
+function zerofree_func()
 {
 	echo "Entering zerofree_func $1"
 	time $ZEROFREE $ZEROFREE_OPTS $1
 }
 
-function main
+function main()
 {
 	echo "Entering main function"
 	mount_nbd "$NBD_DEV $1"
@@ -73,7 +74,7 @@ function main
 }
 
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
-	help_msg
+	print_help
 	exit 0
 elif [ ! -f "$1" ]; then
 	echo "$1 does not exist"
