72f3a1dbfb648518188801539109451d26061630
Tuan T. Pham <tuan@vt.edu>
2016-09-08 04:04:01 -0400

    Add reference to Bunsen Labs script

--- misc/install_pkg.sh
+++ misc/install_pkg.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # __author__: tuan t. pham
+# reference: Bunsen Labs install script
+# https://github.com/BunsenLabs/bunsen-netinstall
+#
 # install debian packages from a file
 # install_pkg.sh package_list
 
