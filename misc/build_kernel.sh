3ca536206f4b3555d3b54841d4b55df0a1827704
Tuan T. Pham <tuan@vt.edu>
2016-11-18 22:30:37 -0500

    Add a script to build kernel
    misc/build_kernel.sh:
    	* Build the kernel from source based on the current config

---
+++ misc/build_kernel.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# Build new kernel from current kernel config
+# tuan t. tpham
+
+BUILD_OUTPUT=${BUILD_OUTPUT:=/tmp/build}
+ARTIFACT=${ARTIFACT:=~/Downloads/kernel}
+OLD_CONFIG=${OLD_CONFIG:=/boot/config-`uname -r`}
+KERNEL_SRC=${KERNEL_SRC:=`pwd`}
+CPUS=${CPUS:=$(grep -c processor /proc/cpuinfo)}
+
+help_msg="\e[1;31mUsage:\e[0m $0
+	Build new kernel from the current config file
+
+	-d: Dump environment variables
+
+	-h|--help: This help messages
+
+\e[1;31mEnvironment Variables:\e[0m
+	\e[1;33mBUILD_OUTPUT\e[0m default=$BUILD_OUTPUT
+		Output directory for object files. One level up is where
+		the .deb files are created.
+
+	\e[1;33mARTIFACT\e[0m default=$ARTIFACT
+		Directory where the .deb files are copied to.
+
+	\e[1;33mOLD_CONFIG\e[0m default=$OLD_CONFIG
+		Old config file.
+
+	\e[1;33mKERNEL_SRC\e[0m default=$KERNEL_SRC
+		Directory of linux kernel source code.
+
+	\e[1;33mCPUS\e[0m  default=$CPUS
+		Number of CPU cores to be used.
+
+\e[1;31mExamples:\e[0m
+	0) Use the default settings
+	$ $0
+
+	1) Set KERNEL_SRC
+	$ KEREL_SRC=/opt/src/linux $0
+
+__author__: tuan t. pham"
+
+function print_help()
+{
+	exec echo -e "$help_msg"
+}
+
+function dump_vars()
+{
+	VAR_LIST="BUILD_OUTPUT ARTIFACT OLD_CONFIG KERNEL_SRC CPUS"
+	for v in $VAR_LIST; do
+		eval echo "$v=\$$v"
+	done
+}
+
+function setup_env()
+{
+	[ -d "$BUILD_OUTPUT" ] || mkdir -p $BUILD_OUTPUT
+	[ -d "$ARTIFACT" ] || mkdir -p $ARTIFACT
+	export KBUILD_OUTPUT=$BUILD_OUTPUT
+	cp $OLD_CONFIG $BUILD_OUTPUT/.config
+	cd $KERNEL_SRC
+	make -s olddefconfig
+	mkdir -p $ARTIFACT/$(make -s kernelversion)
+}
+
+function build_kernel()
+{
+	make -s -j$CPUS
+	make -s -j$CPUS bindeb-pkg
+	cp $BUILD_OUTPUT/../*.deb $ARTIFACT
+}
+
+function main()
+{
+	if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+		print_help
+		exit 1
+	fi
+
+	if [ "$1" = "-d" ]; then
+		dump_vars
+		exit 0
+	fi
+
+	pushd .
+	setup_env
+	build_kernel
+	popd
+}
+
+main "$@"
