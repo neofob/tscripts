fd847a3bbcc3ae765db2e4e6d769a10f166018dd
Tuan T. Pham <tuan@vt.edu>
2017-01-05 23:49:50 -0500

    Add a quick script to do 360

---
+++ photos/mk360.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Add exif info to image file(s) to be "360" friendly on Facebook
+# __author__: tuan t. pham
+
+PTYPE=${PTYPE:="equirectangular"}
+CAM_MAKE=${CAM_MAKE:="RICOH"}
+CAM_MODEL=${CAM_MODEL:="RICOH THETA S"}
+
+for f in "$@"; do
+	echo "Processing $f"
+	exiftool -ProjectionType="$PTYPE" \
+		-Make="$CAM_MAKE" \
+		-Model="$CAM_MODEL" \
+		$f
+done
