fed5d630dc85a69ce5247f639823c6f6420b6530
Tuan T. Pham <tuan@vt.edu>
2018-02-24 23:48:10 -0500

    Add a script to install docker
    docker/install_docker.sh:
    	* Install docker without reading docs.docker.com

---
+++ docker/install_docker.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Just lines of bash codes from https://docs.docker.com/install/linux/docker-ce/ubuntu/
+# Run this as ROOT
+# ./install_docker.sh vagrant
+# vagrant is the username to be added at the end of installtion
+
+apt-get update
+apt-get install \
+	apt-transport-https \
+	ca-certificates \
+	curl \
+	software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) \
+	stable"
+
+apt-get update
+apt-get install docker-ce
+
+# groupadd docker
+usermod -aG docker $1
