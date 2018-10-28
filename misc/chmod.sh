ed1acbf8e740cf1e2c0b057150aa0ac2f163ef65
Tuan T. Pham <tuan@vt.edu>
2018-10-28 14:29:17 -0400

    Update chmod script
    misc/chmod.sh:
        * Update this thing
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- misc/chmod.sh
+++ misc/chmod.sh
@@ -1,37 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# This simple script recursively traverses all subdirs.
-# It chmod 644 files and 755 directories. This script
-# assume that it is in $PATH; so cp it to your ~/bin or
-# something.
+# Change permission of directories and files to 0755 and
+# 0644, respectively.
 # Author: Tuan T. Pham
 
-# It is in DRY_RUN mode by default.
-# Usage:
 # chmod.sh [directory..]
-# To run for real
-# DRY_RUN=0 chmod.sh director
 
-DRY_RUN=${DRY_RUN:=1}
-
-function exec_cmd
-{
-        if [ $DRY_RUN = 0 ]; then
-                eval "$@"
-        else
-                echo "Dry-run, executing..."
-                echo "$@"
-        fi
-}
-
-for file in "$@"; do
-	if [ -d "$file" ]; then
-		(
-		exec_cmd "chmod 755 $file"
-		exec_cmd "cd $file"
-		exec_cmd "$0 *"
-		)
-	else
-		exec_cmd "chmod 644 $file"
-	fi
+for d in $@; do
+	# Directory pass
+	find $d -type d -print | xargs chmod 755
+	# File pass
+	find $d -type f -print | xargs chmod 644
 done
