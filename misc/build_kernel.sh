ab62d42f1bdbbce9c5b64ed91d530aaf900d5b3b
Tuan T. Pham <tuan@vt.edu>
2021-08-11 12:50:59 -0400

    Update some default settings for kernel build
    misc/build_kernel.sh:
        * Disable DEBUG_INFO by default
        * Disable other debugging settings
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- misc/build_kernel.sh
+++ misc/build_kernel.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Build new kernel from current kernel config
 # tuan t. tpham
 
@@ -11,6 +11,15 @@ KERNEL_VERSION=""
 OUTPUT_DIR=""
 SYSTEM_TRUSTED_KEYS=${SYSTEM_TRUSTED_KEYS:=""}
 
+# Set this to "y" if you need debug info
+DEBUG_INFO=${DEBUG_INFO:="n"}
+
+# Some custom settings
+FRAME_POINTER=${FRAME_POINTER:="n"}
+UNWINDER_FRAME_POINTER=${UNWINDER_FRAME_POINTER:="n"}
+KGDB=${KGDB:="n"}
+KGDB_SERIAL_CONSOLE=${KGDB_SERIAL_CONSOLE:="n"}
+
 red="\e[1;31m"
 yellow="\e[1;33m"
 end="\e[0m"
@@ -18,8 +27,8 @@ end="\e[0m"
 help_msg="${red}Usage:${end} $0
 	Build new kernel from the current config file
 
-    Required Debian/Ubuntu packages:
-        * build-essential yacc bison libssl-dev bc
+	Required Debian/Ubuntu packages:
+		* build-essential yacc bison libssl-dev bc
 
 	-d: Dump environment variables
 
@@ -66,7 +75,42 @@ function dump_vars()
 
 function pause()
 {
-   read -p "$*"
+	 read -p "$*"
+}
+
+function set_custom_env()
+{
+		if [ $DEBUG_INFO = "n" ]; then
+			scripts/config --file $BUILD_OUTPUT/.config --disable DEBUG_INFO
+		else
+			scripts/config --file $BUILD_OUTPUT/.config --enable DEBUG_INFO
+		fi
+
+		scripts/config --file $BUILD_OUTPUT/.config --set-str SYSTEM_TRUSTED_KEYS "$SYSTEM_TRUSTED_KEYS"
+
+		if [ $KGDB = "n" ]; then
+			scripts/config --file $BUILD_OUTPUT/.config --disable KGDB
+		else
+			scripts/config --file $BUILD_OUTPUT/.config --enable KGDB
+		fi
+
+		if [ $KGDB_SERIAL_CONSOLE = "n" ]; then
+			scripts/config --file $BUILD_OUTPUT/.config --disable KGDB_SERIAL_CONSOLE
+		else
+			scripts/config --file $BUILD_OUTPUT/.config --enable KGDB_SERIAL_CONSOLE
+		fi
+
+		if [ $FRAME_POINTER = "n" ]; then
+			scripts/config --file $BUILD_OUTPUT/.config --disable FRAME_POINTER
+		else
+			scripts/config --file $BUILD_OUTPUT/.config --enable FRAME_POINTER
+		fi
+
+		if [ $UNWINDER_FRAME_POINTER = "n" ]; then
+			scripts/config --file $BUILD_OUTPUT/.config --disable UNWINDER_FRAME_POINTER
+		else
+			scripts/config --file $BUILD_OUTPUT/.config --enable UNWINDER_FRAME_POINTER
+		fi
 }
 
 function setup_env()
@@ -76,10 +120,10 @@ function setup_env()
 	[ -d "$ARTIFACT" ] || mkdir -p $ARTIFACT
 	export KBUILD_OUTPUT=${BUILD_OUTPUT}
 	cp $OLD_CONFIG ${BUILD_OUTPUT}/.config
-	cd $KERNEL_SRC
+	cd $KERNEL_SRC || exit 1
 	echo "Creating the new config file based on $OLD_CONFIG"
 	make -s olddefconfig
-	scripts/config --file $BUILD_OUTPUT/.config --set-str SYSTEM_TRUSTED_KEYS "$SYSTEM_TRUSTED_KEYS"
+	set_custom_env
 	KERNEL_VERSION=$(make -s kernelversion)
 	OUTPUT_DIR=$ARTIFACT/$KERNEL_VERSION
 	mkdir -p ${OUTPUT_DIR}
@@ -95,10 +139,10 @@ function build_kernel()
 	make -s -j${CPUS} bindeb-pkg
 
 	echo "Copying .deb and .changes files to ${OUTPUT_DIR}"
-    cp ${BUILD_OUTPUT}/../linux-${KERNEL_VERSION}*.changes ${OUTPUT_DIR}
-    cp ${BUILD_OUTPUT}/../linux-{headers,image}-${KERNEL_VERSION}*.deb ${OUTPUT_DIR}
-    cp ${BUILD_OUTPUT}/../linux-libc-dev_${KERNEL_VERSION}*.deb ${OUTPUT_DIR}
-#    cp ${BUILD_OUTPUT}/../linux-firmware-image-${KERNEL_VERSION}*.deb ${OUTPUT_DIR}
+	cp ${BUILD_OUTPUT}/../linux-${KERNEL_VERSION}*.changes ${OUTPUT_DIR}
+	cp ${BUILD_OUTPUT}/../linux-{headers,image}-${KERNEL_VERSION}*.deb ${OUTPUT_DIR}
+	cp ${BUILD_OUTPUT}/../linux-libc-dev_${KERNEL_VERSION}*.deb ${OUTPUT_DIR}
+#	cp ${BUILD_OUTPUT}/../linux-firmware-image-${KERNEL_VERSION}*.deb ${OUTPUT_DIR}
 	echo "DONE!!!"
 }
 
