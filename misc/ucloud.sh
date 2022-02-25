8d438ea9af37c9a935fa56d64325031acf300e6d
Tuan T. Pham <tuan@vt.edu>
2022-02-24 20:00:24 -0500

    Add a script to upload files to remote machines
    misc/ucloud.sh:
        * Upload files to remote machines
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

---
+++ misc/ucloud.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -
+
+PATTERN=${PATTERN:=*.deb}
+LOCAL_DIR=${LOCAL_DIR:=/tmp}
+# replace these hostnames with yours
+REMOTE_HOST=${REMOTE_HOST:="cloud1 cloud2 cloud3"}
+REMOTE_DIR=${REMOTE_DIR:=/tmp}
+for d in $REMOTE_HOST; do
+    echo "Uploading files of pattern $PATTERN to $d"
+    scp $LOCAL_DIR/$PATTERN $d:/$REMOTE_DIR
+done
