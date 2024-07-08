b2c19bf62d9a8b7cff27a6e561dd522beab3ab42
Tuan T. Pham <tuan@vt.edu>
2024-07-08 02:13:25 -0400

    Update whisper-transcribe.sh
    misc/whisper-transcribe.sh:
        * Use realpath to resolve the absolute path for final output file
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- misc/whisper-transcribe.sh
+++ misc/whisper-transcribe.sh
@@ -6,10 +6,12 @@ MODEL=${MODEL:-${WHISPER_MODEL_DIR}/ggml-medium.en.bin}
 SRC_DIR=${SRC_DIR:-.}
 DEST_DIR=${DEST_DIR:-.}
 
+OUTPUT_DIR=$(realpath $DEST_DIR)
+
 for f in $(ls ${SRC_DIR}/*.wav); do
 	base_fn=$(basename $f)
 	out_file="${base_fn%%.*}"
 	#echo $out_file
 	whisper -otxt -l en -m ${MODEL} \
-		-f $f -t 4 -of $DEST_DIR/$out_file
+		-f $f -t 4 -of $OUTPUT_DIR/$out_file
 done
