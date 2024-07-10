cad9422ee5d04bb02401df97322a153b1a3d6d75
Tuan T. Pham <tuan@vt.edu>
2024-07-09 20:00:17 -0400

    Update whisper-transcribe.sh script
    misc/whisper-transcribe.sh:
        * Update how the model file is set
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- misc/whisper-transcribe.sh
+++ misc/whisper-transcribe.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 WHISPER_MODEL_DIR=${WHISPER_MODEL_DIR:-/mnt/extra/src/whisper.cpp/models}
-MODEL=${MODEL:-${WHISPER_MODEL_DIR}/ggml-medium.en.bin}
+MODEL=${MODEL:-ggml-medium.en.bin}
+MODEL_PATH=${WHISPER_MODEL_DIR}/${MODEL}
 
 SRC_DIR=${SRC_DIR:-.}
 DEST_DIR=${DEST_DIR:-.}
@@ -12,6 +13,6 @@ for f in $(ls ${SRC_DIR}/*.wav); do
 	base_fn=$(basename $f)
 	out_file="${base_fn%%.*}"
 	#echo $out_file
-	whisper -otxt -l en -m ${MODEL} \
+	whisper -otxt -l en -m ${MODEL_PATH} \
 		-f $f -t 4 -of $OUTPUT_DIR/$out_file
 done
