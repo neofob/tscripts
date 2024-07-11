3719215a00d6270609fe7eac7afff2adfde8f22f
Tuan T. Pham <tuan@vt.edu>
2024-07-10 23:49:31 -0400

    Add some helper scripts to query privategpt
    text/pgpt_query.py:
        * Query the content of a file with instruct and prompt from
          instruct.yaml
    
    text/query_file.sh:
        * Loop through a bunch of text files to process them
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

---
+++ text/pgpt_query.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# pgpt_query.py --config instruct.yml --input transcript.txt
+# TODO: --output transcript-summary.md
+import argparse
+import os
+import pprint
+import yaml
+
+from pgpt_python.client import PrivateGPTApi
+
+
+pp = pprint.PrettyPrinter(indent=2)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config', dest='config', action='store', help='Config file', required=True)
+parser.add_argument('-i', '--input', dest='input', action='store', help='Input content', required=True)
+#parser.add_argument('-output', '--input', dest='input', action='store', help='Input content', required=True)
+
+config_file = parser.parse_args().config
+input_file = parser.parse_args().input
+
+with open(config_file, "r") as file:
+    my_config = yaml.safe_load(file)
+
+base_url = f"http://{my_config['instruct']['url']}:{int(my_config['instruct']['port'])}"
+
+#pp.pprint(base_url)
+
+#os._exit(0)
+
+client = PrivateGPTApi(base_url=base_url, timeout=my_config['instruct']['timeout'])
+#print(client.health.health())
+
+# form messages
+messages = []
+messages.append({"role": "system", "content": my_config['instruct']['system']})
+
+final_user_text = "<s>[INST] " + my_config['instruct']['instruct']
+content = open(input_file, 'r').read()
+final_user_text = final_user_text + os.linesep + \
+"```" + os.linesep + \
+content + \
+os.linesep + \
+"```" + " [/INST]"
+
+#print(final_user_text)
+
+messages.append({"role": "user", "content": final_user_text })
+# blocking call
+chat_result = client.contextual_completions.chat_completion(messages=messages, timeout=my_config['instruct']['timeout'])
+print(chat_result.choices[0].message.content)
---
+++ text/query_file.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# loop through all files to query privategpt with instruction based on instruct.yml
+# Use pgpt_query.py
+# pip install pgpt_python
+# or install from its source, pip install -e .
+# Sample instruct.yml
+#---
+##instruct.yml
+#instruct:
+#  url: "localhost"
+#  port: 8001
+#  # 8 hours
+#  timeout: 28800
+#  #prompt_file: NONE
+#  #  filename: prompts.yaml
+#  #  index: 42
+#  system: "You are an expert in macroeconomics and world geopolitics."
+#  instruct: "Summarize the following conversation into 5 points."
+#  #textFile:
+#  outputPostfix: "-Summary"
+#  outputExtension: "md"
+
+DEST_DIR=${DEST_DIR:-.}
+OUTPUT_DIR=$(realpath $DEST_DIR)
+INST=${INST:-$HOME/.config/instruct.yml}
+for f in "$@"; do
+	base_fn=$(basename $f)
+	# remove last 12 chars in filename
+	# base_fn="${base_fn::-12}"
+	outfile="${base_fn%%.*}-Summary.md"
+	#echo "$outfile"
+	echo "Processing $f"
+	echo "### Summary" > $OUTPUT_DIR/$outfile
+	echo -n "" >> $outfile
+	time pgpt_query.py -c ${INST} -i $f | fmt --width=80 --split-only >> $outfile
+	echo "Done."
+done
