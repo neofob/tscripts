849cba7f7b1ecfddc904b0a8ecf79c3849010c3c
Tuan T. Pham <tuan@vt.edu>
2025-08-27 15:11:55 -0400

    Add script to download models
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

---
+++ misc/dl_models.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Download Ollama models from a given list
+
+MODELS=${MODELS="devstral:latest"}
+
+for m in ${MODELS}; do
+	time ollama pull $m
+done
+
+printf 'Runtime: %dd:%dh:%dm:%ds\n' "$((SECONDS / 86400))" "$(( (SECONDS % 86400) / 3600 ))" "$(( (SECONDS % 3600) / 60 ))" "$(( SECONDS % 60 ))"
--- misc/whisper-transcribe.sh
+++ misc/whisper-transcribe.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-WHISPER_MODEL_DIR=${WHISPER_MODEL_DIR:-/mnt/extra/src/whisper.cpp/models}
+WHISPER_MODEL_DIR=${WHISPER_MODEL_DIR:-/mnt/extra/models/whisper.cpp}
 MODEL=${MODEL:-ggml-large-v3-turbo.bin}
-MODEL_PATH=${WHISPER_MODEL_DIR}/${MODEL}
+MODEL_PATH=$WHISPER_MODEL_DIR/$MODEL
 
 SRC_DIR=${SRC_DIR:-.}
 DEST_DIR=${DEST_DIR:-.}
@@ -12,7 +12,7 @@ OUTPUT_DIR=$(realpath $DEST_DIR)
 for f in $(ls ${SRC_DIR}/*.wav); do
 	base_fn=$(basename $f)
 	out_file="${base_fn%%.*}"
-	#echo $out_file
+	echo $out_file
 	whisper-cli -otxt -l en -m ${MODEL_PATH} \
 		-f $f -t 4 -of $OUTPUT_DIR/$out_file
 done
