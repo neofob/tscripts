defe0e726a68190133e2afb83634ece6d64fe59c
Tuan T. Pham <tuan@vt.edu>
2025-10-12 19:49:38 -0400

    Update cryptsetup
    blkdev/cryptsetup.sh:
        * cryptsetup version 2.4.3 and earlier uses ripemd160
    
    Signed-off-by: Tuan T. Pham <tuan@vt.edu>

--- blkdev/cryptsetup.sh
+++ blkdev/cryptsetup.sh
@@ -3,17 +3,23 @@
 # https://wiki.archlinux.org/index.php/dm-crypt
 # https://wiki.archlinux.org/index.php/Dm-crypt/Device_encryption
 
+if [ "${DEBUG:-0}" = "1" ]; then
+  set -x
+fi
+
 CONTAINER=${CONTAINER:=container}
 # Available ciphers at /proc/crypto
 # aes-xts-plain64, aes-cbc-essiv...
-CIPHER=${CIPHER:="aes-xts-essiv:sha256"}
-KEY_SIZE=${KEY_SIZE:=512}
-CRYPT_TYPE=${CRYPT_TYPE:=plain}
+CIPHER=${CIPHER:-"aes-xts-essiv:sha256"}
+KEY_SIZE=${KEY_SIZE:-512}
+CRYPT_TYPE=${CRYPT_TYPE:-plain}
+HASH=$HASH:-"ripemd160"}
 # encrypted device = $1
 
 cryptsetup --cipher ${CIPHER} \
-	   --key-size ${KEY_SIZE} \
-	   --type ${CRYPT_TYPE} \
-	   open $1 $CONTAINER
+     --key-size ${KEY_SIZE} \
+     --type ${CRYPT_TYPE} \
+     --hash ${HASH} \
+     open $1 $CONTAINER
 
 echo "Decrypted device available at /dev/mapper/${CONTAINER}"
